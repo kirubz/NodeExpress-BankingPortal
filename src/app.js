9b9f17323d844583043c275d76e3def0326eb740
Kirubaharan Palani <kirubap@cadonix.com>
2018-09-09 04:05:07 +0530

    Completed the module

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const app = express();
 
 const { accounts, users, writeJSON } = require('./data');
 
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -20,45 +23,8 @@ app.get('/', (req, res) => {
     });
 });
 
-app.get('/savings', (req, res) => {
-    res.render('account', {
-        account: accounts.savings
-    });
-});
-
-app.get('/checking', (req, res) => {
-    res.render('account', {
-        account: accounts.checking
-    });
-});
-
-app.get('/credit', (req, res) => {
-    res.render('account', {
-        account: accounts.credit
-    });
-});
-
-app.get('/transfer', (req, res) => {
-    res.render('transfer');
-});
-
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance = accounts[req.body.from].balance = req.body.amount;
-    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
-    writeJSON();
-    res.render('transfer', { message: 'Transfer Completed'});
-});
-
-app.get('/payment', (req, res) => {
-    res.render('payment', {account: accounts.credit});
-})
-
-app.post('/payment', (req, res) => {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount, 10);
-    writeJSON();
-    res.render('payment', { message: 'Payment Completed', account: accounts.credit });
-});
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/profile', (req, res) => {
     res.render('profile', {
